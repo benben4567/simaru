550113c39a98ac094bb51f437ce50e87b9acc477
Benben Bagus P <benben4567@gmail.com>
2022-04-08 01:06:47 +0700

    fix middleware

--- routes/web.php
+++ routes/web.php
@@ -26,39 +26,39 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/chart', [App\Http\Controllers\HomeController::class, 'chart'])->name('chart');
 
-    Route::group(['prefix' => 'validasi', 'as' => 'validasi.'], function(){
+    Route::group(['middleware' => ['can:validasi'], 'prefix' => 'validasi', 'as' => 'validasi.'], function(){
         Route::get('', [App\Http\Controllers\ValidasiController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\ValidasiController::class, 'store'])->name('store');
     });
 
-    Route::group(['prefix' => 'lolos', 'as' => 'lolos.'], function(){
+    Route::group(['middleware' => ['can:lolos'], 'prefix' => 'lolos', 'as' => 'lolos.'], function(){
         Route::get('', [App\Http\Controllers\LolosController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\LolosController::class, 'store'])->name('store');
         Route::get('/maba/show/{id}', [App\Http\Controllers\LolosController::class, 'show'])->name('show');
     });
 
-    Route::group(['prefix' => 'pembayaran', 'as' => 'pembayaran.'], function(){
+    Route::group(['middleware' => ['can:pembayaran'], 'prefix' => 'pembayaran', 'as' => 'pembayaran.'], function(){
         Route::get('', [App\Http\Controllers\PembayaranController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\PembayaranController::class, 'store'])->name('store');
     });
 
-    Route::group(['prefix' => 'rekomendasi', 'as' => 'rekomendasi.'], function(){
+    Route::group(['middleware' => ['can:rekom-internal', 'can:rekom-eksternal'], 'prefix' => 'rekomendasi', 'as' => 'rekomendasi.'], function(){
         Route::post('', [App\Http\Controllers\RekomendasiController::class, 'store'])->name('store');
         Route::get('/internal', [App\Http\Controllers\RekomendasiController::class, 'internal'])->name('internal');
         Route::get('/eksternal', [App\Http\Controllers\RekomendasiController::class, 'eksternal'])->name('eksternal');
     });
 
-    Route::group(['prefix' => 'nim', 'as' => 'nim.'], function(){
+    Route::group(['middleware' => ['can:nim'], 'prefix' => 'nim', 'as' => 'nim.'], function(){
         Route::get('', [App\Http\Controllers\NimController::class, 'index'])->name('index');
     });
 
-    Route::group(['prefix' => 'prodi', 'as' => 'prodi.'], function(){
+    Route::group(['middleware' => ['can:manajemen'], 'prefix' => 'prodi', 'as' => 'prodi.'], function(){
         Route::get('', [App\Http\Controllers\ProdiController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\ProdiController::class, 'store'])->name('store');
         Route::put('', [App\Http\Controllers\ProdiController::class, 'update'])->name('update');
     });
 
-    Route::group(['prefix' => 'pengguna', 'as' => 'pengguna.'], function(){
+    Route::group(['middleware' => ['can:manajemen'], 'prefix' => 'pengguna', 'as' => 'pengguna.'], function(){
         Route::get('', [App\Http\Controllers\UserController::class, 'index'])->name('index');
         Route::put('', [App\Http\Controllers\UserController::class, 'update'])->name('update');
         Route::post('', [App\Http\Controllers\UserController::class, 'store'])->name('store');
