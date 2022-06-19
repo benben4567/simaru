05f50b41bebc02883af266dd570a9542a27d4988
Benben Bagus P <benben4567@gmail.com>
2022-06-19 09:43:57 +0700

    add: Privacy Policy

--- routes/web.php
+++ routes/web.php
@@ -17,28 +17,31 @@ use Illuminate\Support\Facades\Route;
 
 
 Auth::routes([
-  'register' => false, // Register Routes...
-  'reset' => false, // Reset Password Routes...
-  'verify' => false, // Email Verification Routes...
+    'register' => false, // Register Routes...
+    'reset' => false, // Reset Password Routes...
+    'verify' => false, // Email Verification Routes...
 ]);
 
 Route::get('/grafik', [App\Http\Controllers\GrafikController::class, 'index'])->name('grafik');
 Route::get('/rekap', [App\Http\Controllers\GrafikController::class, 'rekap'])->name('rekap');
 Route::get('/export', [App\Http\Controllers\GrafikController::class, 'export'])->name('export');
+Route::get('/privacy-policy', function () {
+    return view('pages.privacy-policy');
+});
 
-Route::group(['middleware' => 'auth'], function(){
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/chart', [App\Http\Controllers\HomeController::class, 'chart'])->name('chart');
     Route::put('/password', [App\Http\Controllers\UserController::class, 'password'])->name('password');
 
-    Route::group(['middleware' => ['permission:validasi'], 'prefix' => 'validasi', 'as' => 'validasi.'], function(){
+    Route::group(['middleware' => ['permission:validasi'], 'prefix' => 'validasi', 'as' => 'validasi.'], function () {
         Route::get('', [App\Http\Controllers\ValidasiController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\ValidasiController::class, 'store'])->name('store');
         Route::post('/update', [App\Http\Controllers\ValidasiController::class, 'update'])->name('update');
         Route::get('/show', [App\Http\Controllers\ValidasiController::class, 'show'])->name('show');
     });
 
-    Route::group(['middleware' => ['permission:lolos'], 'prefix' => 'lolos', 'as' => 'lolos.'], function(){
+    Route::group(['middleware' => ['permission:lolos'], 'prefix' => 'lolos', 'as' => 'lolos.'], function () {
         Route::get('', [App\Http\Controllers\LolosController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\LolosController::class, 'store'])->name('store');
         Route::put('', [App\Http\Controllers\LolosController::class, 'update'])->name('update');
@@ -48,37 +51,37 @@ Route::group(['middleware' => 'auth'], function(){
         Route::get('/maba/show/{id}', [App\Http\Controllers\LolosController::class, 'show'])->name('show');
     });
 
-    Route::group(['middleware' => ['permission:pembayaran'], 'prefix' => 'pembayaran', 'as' => 'pembayaran.'], function(){
+    Route::group(['middleware' => ['permission:pembayaran'], 'prefix' => 'pembayaran', 'as' => 'pembayaran.'], function () {
         Route::get('', [App\Http\Controllers\PembayaranController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\PembayaranController::class, 'store'])->name('store');
         Route::get('/export', [App\Http\Controllers\PembayaranController::class, 'export'])->name('export');
     });
 
-    Route::group(['prefix' => 'rekomendasi', 'as' => 'rekomendasi.'], function(){
+    Route::group(['prefix' => 'rekomendasi', 'as' => 'rekomendasi.'], function () {
         Route::post('', [App\Http\Controllers\RekomendasiController::class, 'store'])->name('store');
         Route::get('/internal', [App\Http\Controllers\RekomendasiController::class, 'internal'])->name('internal')->middleware('permission:rekom-internal');
         Route::get('/eksternal', [App\Http\Controllers\RekomendasiController::class, 'eksternal'])->name('eksternal')->middleware('permission:rekom-eksternal');
         Route::get('/export/{jenis}', [App\Http\Controllers\RekomendasiController::class, 'export'])->name('export')->middleware('permission:rekom-internal|rekom-eksternal');
     });
 
-    Route::group(['middleware' => ['permission:nim'], 'prefix' => 'nim', 'as' => 'nim.'], function(){
+    Route::group(['middleware' => ['permission:nim'], 'prefix' => 'nim', 'as' => 'nim.'], function () {
         Route::get('', [App\Http\Controllers\NimController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\NimController::class, 'store'])->name('store');
     });
 
-    Route::group(['middleware' => ['permission:manajemen'], 'prefix' => 'prodi', 'as' => 'prodi.'], function(){
+    Route::group(['middleware' => ['permission:manajemen'], 'prefix' => 'prodi', 'as' => 'prodi.'], function () {
         Route::get('', [App\Http\Controllers\ProdiController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\ProdiController::class, 'store'])->name('store');
         Route::put('', [App\Http\Controllers\ProdiController::class, 'update'])->name('update');
     });
 
-    Route::group(['middleware' => ['permission:manajemen'], 'prefix' => 'periode', 'as' => 'periode.'], function(){
+    Route::group(['middleware' => ['permission:manajemen'], 'prefix' => 'periode', 'as' => 'periode.'], function () {
         Route::get('', [App\Http\Controllers\PeriodeController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\PeriodeController::class, 'store'])->name('store');
         Route::put('', [App\Http\Controllers\PeriodeController::class, 'update'])->name('update');
     });
 
-    Route::group(['middleware' => ['permission:manajemen'], 'prefix' => 'pengguna', 'as' => 'pengguna.'], function(){
+    Route::group(['middleware' => ['permission:manajemen'], 'prefix' => 'pengguna', 'as' => 'pengguna.'], function () {
         Route::get('', [App\Http\Controllers\UserController::class, 'index'])->name('index');
         Route::put('', [App\Http\Controllers\UserController::class, 'update'])->name('update');
         Route::post('', [App\Http\Controllers\UserController::class, 'store'])->name('store');
@@ -87,11 +90,11 @@ Route::group(['middleware' => 'auth'], function(){
         Route::put('/password', [App\Http\Controllers\UserController::class, 'password'])->name('password');
     });
 
-    Route::group(['middleware' => ['permission:manajemen'], 'prefix' => 'log', 'as' => 'log.'], function(){
+    Route::group(['middleware' => ['permission:manajemen'], 'prefix' => 'log', 'as' => 'log.'], function () {
         Route::get('', [App\Http\Controllers\LogController::class, 'index'])->name('index');
     });
 
-    Route::group(['middleware' => ['permission:validasi'], 'prefix' => 'pendaftar', 'as' => 'pendaftar.'], function(){
+    Route::group(['middleware' => ['permission:validasi'], 'prefix' => 'pendaftar', 'as' => 'pendaftar.'], function () {
         Route::get('', [App\Http\Controllers\PendaftarController::class, 'index'])->name('index');
         Route::post('', [App\Http\Controllers\PendaftarController::class, 'store'])->name('store');
     });
